5faae6dcd1fc8ba8adbe867027f85c7124e6aa03
DAeng0325 <77507262+DAeng0325@users.noreply.github.com>
2025-04-18 03:17:20 +0900

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './lib/**/*.{js,ts,jsx,tsx}'
+    './pages/**/*.{js,ts,jsx,tsx}', 
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {
-      colors: {
-        crtBg: '#0f0f0f',
-        crtText: '#00ffcc'
-      },
-      fontFamily: {
-        mono: ['Courier New', 'monospace']
-      }
-    }
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 }
